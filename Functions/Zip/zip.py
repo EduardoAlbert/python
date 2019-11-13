e62c9b7f470270d13dc683192a0849108286cc44
EduardoAlbert <albert-costa1@live.com>
2019-11-13 15:21:01 -0300

    zip

---
+++ Functions/Zip/zip.py
@@ -0,0 +1,16 @@
+""" zipper1 = [1, 2, 3, 4]
+zipper2 = [5, 6, 7, 8]
+
+print(list(zip(zipper1, zipper2))) """
+
+#--#
+""" 
+print(list(zip('ABCD', 'xy')))
+ """
+
+#--#
+
+d1 = {'a':1, 'b':2}
+d2 = {'c':4, 'd':5}
+
+print(list(zip(d1, d2.values()))
