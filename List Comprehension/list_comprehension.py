70bb0abfd914ed8e9dba56a3df5b3a89c4577b14
EduardoAlbert <albert-costa1@live.com>
2019-11-13 15:01:11 -0300

    list comprehension

--- List Comprehension/list_comprehension.py
+++ List Comprehension/list_comprehension.py
@@ -12,6 +12,7 @@ print(lst)
  """
 
 #--#
+
 """ 
 lst = [ x for x in range(1, 11) if x % 2 == 0 ]
 
@@ -19,6 +20,15 @@ print(lst) """
 
 #--#
 
+""" 
 celsius = [0, 10, 20.1, 34.5]
 
-lst = [  ]
+fahrenheit = [ ((float(9)/5)*temp + 32) for temp in celsius ]
+
+print(fahrenheit) """
+
+#--#
+
+lst = [ x**2 for x in [x**2 for x in range(11)]]
+
+print(lst)
